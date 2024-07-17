798efe7aabe00e33c5de3a79241418c0d07016b2
unknown <yasha1bz@gmail.com>
2024-07-18 00:06:52 +0400

    404 500 pages

--- opinions_app.py
+++ opinions_app.py
@@ -3,7 +3,7 @@
 from datetime import datetime, UTC
 from random import randrange
 
-from flask import Flask, redirect, render_template, url_for, flash
+from flask import Flask, redirect, render_template, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
@@ -47,7 +47,8 @@ class OpinionForm(FlaskForm):
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных записей нет.'
+        # return 'В базе данных записей нет.'
+        abort(500)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
@@ -79,6 +80,26 @@ def add_opinion_view():
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере. 
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, не зафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Пользователю вернётся страница, сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас мы его тоже создадим.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html, и код HTTP-ответа 404:
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
