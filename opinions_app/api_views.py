9d212f17dc452c55d7519b3984f20141faecb402
unknown <yasha1bz@gmail.com>
2024-07-20 15:18:25 +0400

    qnique errors

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -15,11 +15,21 @@ def get_opinion(id):
 
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
+    data = request.get_json()
+    if (
+        'text' in data and 
+        Opinion.query.filter_by(text=data['text']).first() is not None
+    ):
+        # При неуникальном значении поля text
+        # возвращаем сообщение об ошибке в формате JSON
+        # и статус-код 400:
+        return jsonify({'error':
+                        'Такое мнение уже есть в базе данных'}), 400
     opinion = Opinion.query.get_or_404(id)
-    opinion.title = request.json.get('title', opinion.title)
-    opinion.text = request.json.get('text', opinion.text)
-    opinion.source = request.json.get('source', opinion.source)
-    opinion.added_by = request.json.get('added_by', opinion.added_by)
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
     db.session.commit()  
     return jsonify({'opinion': opinion.to_dict()}), 201
 
@@ -41,12 +51,22 @@ def get_opinions():
 
 @app.route('/api/opinions/', methods=['POST'])
 def add_opinion():
-    data = request.get_json()  
-    opinion = Opinion()  
-    opinion.from_dict(data)  
-    db.session.add(opinion)  
-    db.session.commit()  
-    return jsonify({'opinion': opinion.to_dict()}), 201
+    data = request.get_json()
+    if 'title' not in data or 'text' not in data:
+        return jsonify({'error':
+                        'В запросе отсутствуют обязательные поля'}), 400
+    # Если в базе данных уже есть объект 
+    # с таким же значением поля text...
+    if Opinion.query.filter_by(text=data['text']).first() is not None:
+        # ...возвращаем сообщение об ошибке в формате JSON 
+        # и статус-код 400:
+        return jsonify({'error': 
+                        'Такое мнение уже есть в базе данных'}), 400
+    opinion = Opinion()
+    opinion.from_dict(data)
+    db.session.add(opinion)
+    db.session.commit()
+    return jsonify({'opinion': opinion.to_dict()}), 201 
 
 
 @app.route('/api/get-random-opinion/', methods=['GET'])
